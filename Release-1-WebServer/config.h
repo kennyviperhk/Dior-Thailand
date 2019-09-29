8149a0fd5850a5671a2a7acc7a098c6a06179b03
Kenny Wong <kennyviperhk@gmail.com>
2019-09-29 17:04:59 +0800

    update

---
+++ Release-1-WebServer/config.h
@@ -0,0 +1,16 @@
+
+
+#define   STATION_SSID     "KakaLeung_Home"
+#define   STATION_PASSWORD "27823034"
+
+#define HOSTNAME "HTTP_BRIDGE"
+
+const char* http_username = "admin";
+const char* http_password = "admin";
+
+#define   STATION_PORT     5555
+uint8_t   station_ip[4] =  {192,168,0,140}; // IP of the server
+
+#define   MESH_PREFIX     "whateverYouLike"
+#define   MESH_PASSWORD   "somethingSneaky"
+#define   MESH_PORT       5555
--- WebServerWsDior/config.h
+++
@@ -1,9 +0,0 @@
-const char* ssid = "KakaLeung_Home";
-const char* password = "27823034";
-const char * hostName = "esp-async";
-const char* http_username = "admin";
-const char* http_password = "admin";
-
-#define   MESH_PREFIX     "whateverYouLike"
-#define   MESH_PASSWORD   "somethingSneaky"
-#define   MESH_PORT       5555
